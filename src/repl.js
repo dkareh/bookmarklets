21572e25fe47b323912a38e1c5e22c0b74b83b80
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-09-07 08:55:37 -0400

    REPL: Recursively inspect object contents
    Instead of outputting '[object Object]', we now recursively print or
    'inspect' the contents of any object. Furthermore, the bookmarklet now
    clearly marks empty array slots as '<empty>'.

--- src/repl.js
+++ src/repl.js
@@ -1,4 +1,47 @@
 (() => {
+    const inspectors = {
+        bigint: (bigint) => bigint.toString() + "n",
+        boolean: (boolean) => boolean.toString(),
+        function: (func) => func.toString(),
+        number: (number) => number.toString(),
+        object: inspectObject,
+        string: (string) => string.toString(),
+        symbol: (symbol) => symbol.toString(),
+        undefined: () => "undefined",
+    };
+
+    function inspect(value) {
+        return inspectors[typeof value](value);
+    }
+
+    function inspectObject(object) {
+        // The type of `null` is "object", so handle it here.
+        if (object === null) return "null";
+
+        // Handle special objects.
+        if (Array.isArray(object)) return inspectArray(object);
+
+        // Handle regular objects.
+        const props = Object.entries(object).map(inspectProp).join(", ");
+        return props == "" ? `{ }` : `{ ${props} }`;
+    }
+
+    function inspectProp([key, value]) {
+        return `${key}: ${inspect(value)}`;
+    }
+
+    function inspectArray(array) {
+        // Don't output two spaces in an empty array.
+        if (array.length == 0) return "[ ]";
+
+        // Output all slots, including empty slots.
+        const slots = [];
+        for (let i = 0; i < array.length; ++i) {
+            slots[i] = i in array ? inspect(array[i]) : "<empty>";
+        }
+        return `[ ${slots.join(", ")} ]`;
+    }
+
     for (let code = `[..."Hello! \\u{1F600}"].join("·")`; ; ) {
         // Read the next block of code to evaluate.
         code = prompt("Enter code:", code);
@@ -8,7 +51,7 @@
         let result;
         try {
             // Use `?.` (optional chaining) to force indirect `eval`.
-            result = "✔ " + eval?.(`"use strict"; ${code}`);
+            result = "✔ " + inspect(eval?.(`"use strict"; ${code}`));
         } catch (error) {
             result = "✘ " + error;
         }
