79731c9a7f0bdcf9264906915bcd816cf7e54aed
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-09-14 15:33:01 -0400

    Export: Output correctly indented HTML

--- build/export.zig
+++ build/export.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const assert = std.debug.assert;
 const fs = std.fs;
 const ziggy = @import("ziggy");
 const Map = ziggy.dynamic.Map;
@@ -86,13 +87,23 @@ fn exportTree(
 }
 
 const Export = struct {
+    const indent_size = 2;
+    const State = enum {
+        begin_data,
+        in_attributes,
+        in_internal_element,
+        in_leaf_element,
+    };
+
     arena: std.mem.Allocator,
     writer: std.io.AnyWriter,
     root_path: []const u8,
+    nesting_level: usize = initial_nesting_level,
+    state: State = initial_state,
 
-    pub fn exportRoot(self: Export, metadata: ?Bookmarklets) !void {
+    pub fn exportRoot(self: *Export, metadata: ?Bookmarklets) !void {
         try self.writer.writeAll(header);
-        try self.exportSubDir(fs.cwd(), self.root_path, .{
+        try self.exportDir(fs.cwd(), self.root_path, .{
             .title = "Bookmarklets",
             .items = if (metadata) |m| m.items else .{},
         });
@@ -101,46 +112,105 @@ const Export = struct {
 
     const Error = anyerror;
 
-    // FIXME: Indent HTML correctly.
-    fn exportSubDir(self: Export, dir: fs.Dir, path: []const u8, metadata: ?Folder) Error!void {
+    fn exportDir(self: *Export, dir: fs.Dir, path: []const u8, metadata: ?Folder) Error!void {
         var sub_dir = try dir.openDir(path, .{ .iterate = true });
         defer sub_dir.close();
 
-        try self.writer.writeAll("  <DT>\n    <H3>");
-        try self.exportName(if (metadata) |m| m.title else path);
-        try self.writer.writeAll("</H3>\n    <DL>\n");
-        try self.exportDir(sub_dir, if (metadata) |m| m.items else .{});
-        try self.writer.writeAll("    </DL>\n  </DT>\n");
+        try self.openTag("DT");
+        try self.openTag("H3");
+        try self.data(if (metadata) |m| m.title else path);
+        try self.closeTag("H3");
+        try self.openTag("DL");
+        try self.exportDirEntries(sub_dir, if (metadata) |m| m.items else .{});
+        try self.closeTag("DL");
+        try self.closeTag("DT");
     }
 
-    fn exportDir(self: Export, dir: fs.Dir, items: Map(Item)) !void {
+    fn exportDirEntries(self: *Export, dir: fs.Dir, items: Map(Item)) !void {
         var iterator = dir.iterate();
         const fields = items.fields;
         while (try iterator.next()) |entry| {
             const name = entry.name;
             const metadata = fields.get(name);
             switch (entry.kind) {
-                .directory => try self.exportSubDir(dir, name, try Folder.from(metadata)),
+                .directory => try self.exportDir(dir, name, try Folder.from(metadata)),
                 .file => try self.exportFile(dir, name, try Bookmarklet.from(metadata)),
                 else => continue,
             }
         }
     }
 
-    // FIXME: Indent HTML correctly.
-    fn exportFile(self: Export, dir: fs.Dir, path: []const u8, metadata: ?Bookmarklet) !void {
+    fn exportFile(self: *Export, dir: fs.Dir, path: []const u8, metadata: ?Bookmarklet) !void {
         const max_bytes = std.math.maxInt(usize);
         const link = try dir.readFileAlloc(self.arena, path, max_bytes);
-        try self.writer.writeAll("      <DT>\n        <A HREF=\"");
-        try self.exportLink(link);
-        try self.writer.writeAll("\">");
-        try self.exportName(if (metadata) |m| m.title else path);
-        try self.writer.writeAll("</A>\n      </DT>\n");
+        try self.openTag("DT");
+        try self.openTagAndAttributes("A");
+        try self.attribute("HREF", link);
+        try self.finishAttributes();
+        try self.data(if (metadata) |m| m.title else path);
+        try self.closeTag("A");
+        try self.closeTag("DT");
+    }
+
+    fn stateIsOneOf(self: Export, states: []const State) bool {
+        return std.mem.indexOfScalar(State, states, self.state) != null;
+    }
+
+    fn nextLine(self: Export) !void {
+        try self.writer.writeByte('\n');
+        try self.writer.writeByteNTimes(' ', self.nesting_level * indent_size);
+    }
+
+    /// Assumes that `name` is a legal HTML tag name.
+    fn openTag(self: *Export, name: []const u8) !void {
+        try self.openTagAndAttributes(name);
+        try self.finishAttributes();
+    }
+
+    /// Assumes that `name` is a legal HTML tag name.
+    fn openTagAndAttributes(self: *Export, name: []const u8) !void {
+        assert(self.stateIsOneOf(&.{ .begin_data, .in_internal_element }));
+        try self.nextLine();
+        try self.writer.print("<{s}", .{name});
+        self.nesting_level += 1;
+        self.state = .in_attributes;
+    }
+
+    /// Assumes that `name` is a legal HTML tag name.
+    /// Assumes that `self.nesting_level` is greater than zero.
+    fn closeTag(self: *Export, name: []const u8) !void {
+        assert(self.stateIsOneOf(&.{ .begin_data, .in_internal_element, .in_leaf_element }));
+        self.nesting_level -= 1;
+        // Put closing tags for non-leaf elements on their own lines.
+        if (self.state == .in_internal_element)
+            try self.nextLine();
+        try self.writer.print("</{s}>", .{name});
+        self.state = .in_internal_element;
+    }
+
+    /// Assumes that `name` is a legal HTML attribute name.
+    fn attribute(self: Export, name: []const u8, value: []const u8) !void {
+        assert(self.state == .in_attributes);
+        try self.writer.print(" {s}=\"", .{name});
+        try self.escapeAttributeValue(value);
+        try self.writer.writeByte('"');
+    }
+
+    fn finishAttributes(self: *Export) !void {
+        assert(self.state == .in_attributes);
+        try self.writer.writeByte('>');
+        self.state = .begin_data;
+    }
+
+    fn data(self: *Export, bytes: []const u8) !void {
+        assert(self.stateIsOneOf(&.{ .begin_data, .in_leaf_element }));
+        try self.escapeData(bytes);
+        self.state = .in_leaf_element;
     }
 
     // https://html.spec.whatwg.org/multipage/parsing.html#data-state
-    fn exportName(self: Export, name: []const u8) !void {
-        var remaining = name;
+    fn escapeData(self: Export, bytes: []const u8) !void {
+        var remaining = bytes;
         while (std.mem.indexOfAny(u8, remaining, "&<")) |index| {
             try self.writer.writeAll(remaining[0..index]);
             try self.writer.writeAll(htmlEscape(remaining[index]));
@@ -150,8 +220,8 @@ const Export = struct {
     }
 
     // https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(double-quoted)-state
-    fn exportLink(self: Export, link: []const u8) !void {
-        var remaining = link;
+    fn escapeAttributeValue(self: Export, value: []const u8) !void {
+        var remaining = value;
         while (std.mem.indexOfAny(u8, remaining, "\"&")) |index| {
             try self.writer.writeAll(remaining[0..index]);
             try self.writer.writeAll(htmlEscape(remaining[index]));
@@ -171,14 +241,18 @@ const Export = struct {
         };
     }
 
+    // Initially, there's only one open element: `<DL>`.
+    const initial_nesting_level = 1;
+    const initial_state: State = .begin_data;
+
     const header =
         \\<!DOCTYPE NETSCAPE-Bookmark-file-1>
         \\<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8" />
         \\<DL>
-        \\
     ;
 
     const footer =
+        \\
         \\</DL>
     ;
 };
