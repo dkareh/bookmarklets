c26f87777f2381ff5f3000f7349ef8476409b332
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-09-15 16:20:31 -0400

    Google: Manually specify the translation language
    Letting Google guess the language to translate to is convenient, but
    it introduces an additional history entry as Google Translate updates
    the current URL when it determine's the user's preferred language. The
    consequence is that the browser's back button has no apparent effect
    unless it is pressed twice in quick succession.

--- src/google/translate.js
+++ src/google/translate.js
@@ -1,6 +1,10 @@
 (() => {
     const text = prompt("Enter text:");
     if (!text) return;
-    const prefix = "https://translate.google.com/?text=";
-    location.assign(prefix + encodeURIComponent(text));
+    const prefix = "https://translate.google.com/?op=translate&sl=auto&tl=";
+    // Google Translate usually only needs the primary language subtag.
+    // https://datatracker.ietf.org/doc/html/rfc5646#section-2.1
+    const tag = navigator.languages?.[0] ?? navigator.language;
+    const primaryLanguage = tag.match(/^[A-Za-z]{2,8}/)?.[0] ?? tag;
+    location.assign(prefix + primaryLanguage + "&text=" + encodeURIComponent(text));
 })();
--- src/google/translate_selection.js
+++ src/google/translate_selection.js
@@ -1,5 +1,9 @@
 (() => {
     const text = getSelection().toString();
-    const prefix = "https://translate.google.com/?text=";
-    location.assign(prefix + encodeURIComponent(text));
+    const prefix = "https://translate.google.com/?op=translate&sl=auto&tl=";
+    // Google Translate usually only needs the primary language subtag.
+    // https://datatracker.ietf.org/doc/html/rfc5646#section-2.1
+    const tag = navigator.languages?.[0] ?? navigator.language;
+    const primaryLanguage = tag.match(/^[A-Za-z]{2,8}/)?.[0] ?? tag;
+    location.assign(prefix + primaryLanguage + "&text=" + encodeURIComponent(text));
 })();
