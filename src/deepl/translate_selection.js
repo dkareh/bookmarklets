f83ae423055d68f31499f2f1562238f4b163c0ba
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-09-15 17:03:19 -0400

    DeepL: Manually specify the translation language

--- src/deepl/translate_selection.js
+++ src/deepl/translate_selection.js
@@ -1,7 +1,8 @@
 (() => {
     const text = getSelection().toString();
-    // NOTE: The hash doesn't have to be "#auto/auto/...", but that choice is
+    // NOTE: The hash doesn't have to be "#auto/...", but that choice is
     // the most self-explanatory: we want DeepL to infer the languages.
-    const prefix = "https://www.deepl.com/translator#auto/auto/";
-    location.assign(prefix + encodeURIComponent(text));
+    const prefix = "https://www.deepl.com/translator#auto/";
+    const tag = navigator.languages?.[0] ?? navigator.language;
+    location.assign(prefix + tag + "/" + encodeURIComponent(text));
 })();
