bcbb48e3d40307d5a5ef3a0b4bea438b693cd9fb
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-12-17 15:32:44 -0500

    DeepL: Update bookmarklets
    The original URL template no longer works without the source language.
    By reading the main page's HTML, I discovered that DeepL supports a
    different template that works even without the source language tag.

--- src/deepl/translate.js
+++ src/deepl/translate.js
@@ -1,9 +1,18 @@
 (() => {
-    const text = prompt("Enter text:");
+    // These characters are forbidden because they cause various issues:
+    // - Line terminators don't match a `.` regex used by DeepL.
+    // - '%' causes a `decodeURIComponent` call to throw a `URIError`.
+    // - '.' causes DeepL to send a '404 Not Found' response.
+    // - '/' is misinterpreted as a segment separator.
+    // Percent-encoding doesn't resolve these issues, so we must remove the
+    // characters. Just deleting them would merge adjacent words such as 'and/
+    // or', so replace each character by a single space instead.
+    const forbiddenCharacterRegex = /[\n\r\u2028\u2029%./]/g;
+    const text = prompt("Enter text:")?.replaceAll(forbiddenCharacterRegex, " ");
     if (!text) return;
-    // NOTE: The hash doesn't have to be "#auto/...", but that choice is
-    // the most self-explanatory: we want DeepL to infer the languages.
-    const prefix = "https://www.deepl.com/translator#auto/";
+    // To make DeepL detect the language, we use '_' instead of a real language
+    // tag. (I originally used 'detect', but DeepL thought I meant German.)
+    const prefix = "https://www.deepl.com/translator/q/_/";
     const tag = navigator.languages?.[0] ?? navigator.language;
-    location.assign(prefix + tag + "/" + encodeURIComponent(text));
+    location.assign(prefix + encodeURIComponent(text) + "/" + tag);
 })();
