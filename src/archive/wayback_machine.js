de60ecaae7ff59b1316691829f62feeafb555267
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2025-04-05 17:02:06 -0400

    Archive: Simplify the Wayback Machine bookmarklet

--- src/archive/wayback_machine.js
+++ src/archive/wayback_machine.js
@@ -1,10 +1,10 @@
-(() => {
-    // https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format
-    const iso8601 = new Date().toISOString();
-    const timestamp = iso8601
-        .replace(/^[+-]\d\d/, "") // Truncate expanded years ('±YYYYYY').
-        .replaceAll(/\D/g, "") // Remove the delimiters (non-digits).
-        .replace(/\d\d\d$/, ""); // Remove the number of milliseconds.
-    const prefix = `https://web.archive.org/web/${timestamp}/`;
-    location.assign(prefix + encodeURIComponent(location.href));
-})();
+// The Wayback Machine allows you to omit digits from the timestamp. By writing
+// '2', we request the latest archive from 2000/01/01 00:00:00 to the current
+// year, 12/31 23:59:59. A few Internet Archive projects depend on the behavior:
+// - https://github.com/internetarchive/wayback-machine-ios/blob/1d15a454/WM/Classes/Global/WMSAPIManager.swift#L48
+// - https://github.com/internetarchive/wayback-machine-webextension/blob/30b8430e/webextension/scripts/background.js#L1222
+// - https://github.com/internetarchive/wayback-machine-webextension/blob/30b8430e/webextension/scripts/popup.js#L238
+// NOTE: It probably won't happen, but I'm assuming the Wayback Machine will
+// fall back to returning an archive from 1996 to 1999 if there's no newer one.
+// Source: https://github.com/internetarchive/wayback/blob/a331f968/wayback-core/src/main/java/org/archive/wayback/archivalurl/requestparser/ReplayRequestParser.java#L60-L65
+location.assign(`https://web.archive.org/web/2/${encodeURIComponent(location.href)}`);
