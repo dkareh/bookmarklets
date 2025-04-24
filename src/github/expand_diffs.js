cf3a20634b19f6407c52bfc40b267fe599fe7d5c
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2025-04-24 17:34:57 -0400

    Add GitHub bookmarklets to collapse and expand diffs

---
+++ src/github/expand_diffs.js
@@ -0,0 +1,6 @@
+// Based on https://lobste.rs/s/pb8bq4/bookmarklets#c_g0gwtj
+for (const button of document.querySelectorAll(
+    "button[aria-label='Toggle diff contents'][aria-expanded='false']",
+)) {
+    button.click();
+}
