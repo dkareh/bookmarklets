5b5004a823c9df593617ca969296eaced958a655
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2025-04-24 17:34:57 -0400

    Add bookmarklets to test webpage accessibility/validity
    `evaluate_accessibility_wave.js` hands the current page over to the
    WebAIM WAVE web accessibility evaluation tool; `validate_html_nu.js` and
    `validate_html_w3c.js` both validate the current page's HTML.
    
    P.S.: The W3C validator seems to be a newer version of the Nu validator.

---
+++ src/web_development/evaluate_accessibility_wave.js
@@ -0,0 +1,2 @@
+// Based on https://lobste.rs/s/vjdc18/bookmarklets#c_jugd46
+location.assign(`https://wave.webaim.org/report#/${location}`);
---
+++ src/web_development/validate_html_w3c.js
@@ -0,0 +1,2 @@
+// Based on https://lobste.rs/s/vjdc18/bookmarklets#c_jugd46
+location.assign(`https://validator.w3.org/nu/?doc=${encodeURIComponent(location)}`);
