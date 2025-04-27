ec9626abae3dc7bae14341a25f2095f249dd7e04
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2025-04-27 00:18:38 -0400

    Convert: Support decoding of URL-safe Base64

--- src/convert/base64_to_bytes.js
+++ src/convert/base64_to_bytes.js
@@ -3,7 +3,7 @@
     if (!base64) return;
 
     // https://developer.mozilla.org/en-US/docs/Glossary/Base64
-    const byteString = atob(base64);
+    const byteString = atob(base64.replaceAll("-", "+").replaceAll("_", "/"));
     const bytes = Array.from(byteString, (point) => point.codePointAt(0));
     const toString = (byte) => byte.toString(16).padStart(2, "0");
     alert(bytes.map(toString).join(" ").toUpperCase());
