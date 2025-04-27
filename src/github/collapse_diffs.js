cb4007c74e05f512cbe26918dfcb9c358a0f542c
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2025-04-27 00:18:38 -0400

    GitHub: Collapse and expand diffs in individual commits

--- src/github/collapse_diffs.js
+++ src/github/collapse_diffs.js
@@ -1,6 +1,9 @@
 // Based on https://lobste.rs/s/pb8bq4/bookmarklets#c_g0gwtj
 for (const button of document.querySelectorAll(
-    "button[aria-label='Toggle diff contents'][aria-expanded='true']",
+    [
+        "button[aria-label='Toggle diff contents'][aria-expanded='true']",
+        "button[aria-label^='Collapse file: ' i]",
+    ].join(", "),
 )) {
     button.click();
 }
--- src/github/expand_diffs.js
+++ src/github/expand_diffs.js
@@ -1,6 +1,9 @@
 // Based on https://lobste.rs/s/pb8bq4/bookmarklets#c_g0gwtj
 for (const button of document.querySelectorAll(
-    "button[aria-label='Toggle diff contents'][aria-expanded='false']",
+    [
+        "button[aria-label='Toggle diff contents'][aria-expanded='false']",
+        "button[aria-label^='Expand file: ' i]",
+    ].join(", "),
 )) {
     button.click();
 }
