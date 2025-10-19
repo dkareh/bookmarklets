7ae60d29b7415ce71f67c8368cfe927363e72b7c
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2025-10-19 01:17:00 -0400

    Archive: Simplify the Wayback Machine bookmarklet

--- src/archive/wayback_machine.js
+++ src/archive/wayback_machine.js
@@ -1,10 +1,3 @@
-(() => {
-    // https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format
-    const iso8601 = new Date().toISOString();
-    const timestamp = iso8601
-        .replace(/^[+-]\d\d/, "") // Truncate expanded years ('±YYYYYY').
-        .replaceAll(/\D/g, "") // Remove the delimiters (non-digits).
-        .replace(/\d\d\d$/, ""); // Remove the number of milliseconds.
-    const prefix = `https://web.archive.org/web/${timestamp}/`;
-    location.assign(prefix + encodeURIComponent(location.href));
-})();
+// Request the latest copy by omitting the timestamp.
+// Source: https://archive.org/web/web-advancedsearch.php
+location.assign(`https://web.archive.org/web/${encodeURIComponent(location)}`);
